81624a2560a2b296913f17bf9820636353d2b1cf
Katzuuu <dodookormos@gmail.com>
2025-07-18 19:15:07 +0200

    glfw input handler

--- Window.cpp
+++ Window.cpp
@@ -4,11 +4,13 @@ Window::Window(): bufferWidth(0), bufferHeight(0) {
     winWidth = 800;
     winHeight = 600;
     window = nullptr;
+    memset(keys, false, sizeof(keys));
 }
 
 Window::Window(const GLint winWidth, GLint winHeight)
     : winWidth(winWidth), winHeight(winHeight), bufferWidth(0), bufferHeight(0) {
     window = nullptr;
+    memset(keys, false, sizeof(keys));
 }
 
 void Window::Initialize() {
@@ -34,6 +36,9 @@ void Window::Initialize() {
     glfwSetWindowAttrib(window, GLFW_RESIZABLE, GL_FALSE);
     glfwMakeContextCurrent(window);
 
+    // handle key + mouse input
+    CreateCallbacks();
+
     // initialization of GLAD
     if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
         throw std::runtime_error("Failed to initialize GLAD");
@@ -41,6 +46,30 @@ void Window::Initialize() {
     glfwGetFramebufferSize(window, &bufferWidth, &bufferHeight);
     glViewport(0, 0, bufferWidth, bufferHeight);
     glEnable(GL_DEPTH_TEST);
+
+    glfwSetWindowUserPointer(window, this);
+}
+
+void Window::HandleKeys(GLFWwindow *window, int key, int code, int action, int mode) {
+    Window *self = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+
+    if (key >= 0 && key < 1024) {
+        if (action == GLFW_PRESS) {
+            self->keys[key] = true;
+            std::cout << "Pressed " << key << "\n";
+        } else if (action == GLFW_RELEASE) {
+            self->keys[key] = false;
+            std::cout << "Released " << key << "\n";
+        }
+    }
+}
+
+void Window::CreateCallbacks() const {
+    glfwSetKeyCallback(window, HandleKeys);
 }
 
 Window::~Window() {
--- Window.h
+++ Window.h
@@ -1,6 +1,7 @@
 # pragma once
 
 #include <iostream>
+#include <string.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -8,6 +9,7 @@ class Window {
     GLfloat winWidth, winHeight;
     GLint bufferWidth, bufferHeight;
     GLFWwindow* window;
+    bool keys[1024];
 public:
     Window();
     Window(GLint winWidth, GLint winHeight);
@@ -18,4 +20,7 @@ public:
     bool GetShouldClose() const { return glfwWindowShouldClose(window); };
     void SwapBuffers() const { glfwSwapBuffers(window); };
     void PollEvents() const { glfwPollEvents(); };
+private:
+    static void HandleKeys(GLFWwindow *window, int key, int code, int action, int mode);
+    void CreateCallbacks() const;
 };
